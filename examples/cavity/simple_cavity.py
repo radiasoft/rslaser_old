de326f6be85741484d7ce97190a86e03c5a7250a
David Bruhwiler <bruhwiler@radiasoft.net>
2021-11-01 18:46:59 +0000

    updated to work with agg backend, plus comments

--- examples/cavity/simple_cavity.py
+++ examples/cavity/simple_cavity.py
@@ -1,28 +1,49 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+u"""Propagation of a wavefront in an optical cavity
 
-import rslaser.rsoptics
-from rslaser.rsoptics.element import *
+The initial wavefront is a Hermite-Gaussian mode located at the center of a cavity
+of length L_cav. There is a crystal located at the center of the cavity of length L_cryst.
+The wavefront is propagated through the crystal and then to the end of the cavity.
+
+:copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+
+from __future__ import absolute_import, division, print_function
+
 import rslaser.rscavity
 from rslaser.rscavity.laser_cavity import *
 
+import rslaser.rsoptics
+from rslaser.rsoptics.element import *
+
 import matplotlib
 from matplotlib import pyplot
+# matplotlib.use("agg")  # for testing purposes
+
 import numpy as np
-import time
+
 from pykern.pkcollections import PKDict
+import time
 
-# This script propagates a wavefront through an optical cavity.
-# The initial wavefront is a Hermite-Gaussian mode located at the center of a cavity
-# of length L_cav. There is a crystal located at the center of the cavity of length L_cryst.
-# The wavefront is propagated through the crystal and then to the end of the cavity.
+def pkplot(filename):
+    if matplotlib.get_backend() == 'agg':
+        filename = 'img01.png'
+        pyplot.savefig(filename)
+        pyplot.close()
+    else:
+        pyplot.show()
 
+print(" ")
+print("Define laser pulse, crystal and cavity parameters...")
+
+# local Pykern dictionary object to hold all physical parameters
 k=PKDict(
-# Define Cavity length
-L_cav = 1, #Length of cavity [m]
+  L_cav = 1, #Length of cavity [m]
+  df = 0.3, #Focal length difference from confocal case [m]
+)
 
 # Define right and left mirror focal lengths
-df = 0.3, #Focal length difference from confocal case [m]
-)
 k.dfR = k.df
 k.dfL = k.df
 k.f=k.L_cav/4+k.df #focal length
@@ -31,9 +52,9 @@ k.lens_left_focal_length = k.f
 k.lens_right_focal_length = k.f
 
 #Define Crystal parameters
-k.L_cryst = 0.1 #Length of crystal [m]
-k.n0=1.75 #index of refraction on axis
-k.n2=0.0 #radial variation of index of refraction: n(r) = n0 - 0.5 n2 r^2
+k.L_cryst = 0.1   # Length of crystal [m]
+k.n0=1.75         # index of refraction on axis
+k.n2=0.0          # radial variation of index of refraction: n(r) = n0 - 0.5 n2 r^2
 k.drift_right_length=k.L_cav/2-k.L_cryst/2
 k.drift_left_length=k.drift_right_length
 
@@ -55,72 +76,89 @@ k.sig_s=0.1 #rms length of Gaussian laser pulse [m]
 
 k.L_half_cryst=k.L_cryst/2
 
-
-#create laser cavity
+print(" ")
+print("Initialize the laser cavity object...")
 lc = LaserCavity(k)
 
-#nslice=nslice,L_half_cryst=L_cryst/2,
-#                        drift_right_length=drift_right_length,drift_left_length=drift_left_length,
-#                        lens_left_focal_length=lens_left_focal_length,
-#                        lens_right_focal_length=lens_right_focal_length,n0=n0,n2=n2,sigrW=sigx0
-
-#output initial parameters
 svals = lc.laser_pulse.pulsePos()
 (lpsxvals,lpsyvals) = lc.laser_pulse.rmsvals()
 ivals = lc.laser_pulse.intensity_vals()
 evals = lc.laser_pulse.energyvals()
 
-# set the backend
-matplotlib.use('tkagg')
+print(" ")
+print("Plot the RMS values along the initial laser pulse...")
 
-#Plot rms values along laser pulse
+pyplot.title('RMS Beam size along laser pulse')
 pyplot.plot(svals,lpsyvals)
 pyplot.xlabel('s [m]')
 pyplot.ylabel('rms x [m]')
-pyplot.title('RMS Beam size along laser pulse')
-pyplot.show()
 
-#Plot pulse intensity along laser pulse
+pkplot('img01.png')
+
+print(" ")
+print("Plot the pulse intensity along the initial laser pulse...")
+
 pyplot.plot(svals,ivals)
 pyplot.ylabel('pulse intensity []')
 pyplot.xlabel('s [m]')
-pyplot.show()
+
+pkplot('img02.png')
+
+print(" ")
+print("Plot the slice energy along the initial laser pulse...")
 
 pyplot.title('Slice energy along laser pulse')
 pyplot.plot(svals,evals)
 pyplot.xlabel('s [m]')
 pyplot.ylabel('Energy [ev]')
-pyplot.show()
+
+pkplot('img03.png')
+
+print(" ")
+print("Propagate the laser pulse several times through the cavity....")
 
 start_time = time.time()
 (svals, sxvals, syvals) = lc.propagate(num_cycles=4)
 
-fig, ax = pyplot.subplots()
-ax.plot(svals, sxvals)
-pyplot.show()
 print("Simulation time: %s seconds" %(round((time.time() - start_time),5)))
 
-#get final laser pulse rms values and intensity values along pulse
+print(" ")
+print("Plot the transverse size of the pulse as it oscillates back and forth ...")
+
+fig, ax = pyplot.subplots()
+ax.plot(svals, sxvals)
+
+pkplot('img04.png')
+
+print(" ")
+print("Plot the RMS values along the final laser pulse...")
+
 svals = lc.laser_pulse.pulsePos()
 (lpsxvals,lpsyvals) = lc.laser_pulse.rmsvals()
 ivals = lc.laser_pulse.intensity_vals()
 evals = lc.laser_pulse.energyvals()
 
-#Plots
 pyplot.title('RMS Beam size along laser pulse')
 pyplot.plot(svals,lpsyvals)
 pyplot.xlabel('s [m]')
 pyplot.ylabel('rms x [m]')
-pyplot.show()
 
+pkplot('img05.png')
+
+print(" ")
+print("Plot the laser intensity along the final laser pulse...")
 pyplot.title('Intensity along laser pulse')
 pyplot.plot(svals,ivals)
 pyplot.ylabel('pulse intensity []')
 pyplot.xlabel('s [m]')
-pyplot.show()
 
+pkplot('img06.png')
+
+print(" ")
+print("Plot the slice energy along the final laser pulse...")
 pyplot.title('Slice energy along laser pulse')
 pyplot.plot(svals,evals)
 pyplot.xlabel('s [m]')
 pyplot.ylabel('Energy [ev]')
-pyplot.show()
+
+pkplot('img07.png')
