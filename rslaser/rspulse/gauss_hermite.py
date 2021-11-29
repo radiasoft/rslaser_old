fixed bug in zR calculation; now diverging envelopes have correct phase behavior

rslaser/rspulse/gauss_hermite.py
@@ -55,7 +55,8 @@ class GaussHermite:
         self.z_waist_y = _k.dzwy                  # location (in z) of vertical waist
         
         # Rayleigh range
-        self.zR = 0.5*self.k0*self.w0*2           # Rayleigh range, ignoring horizontal/vertical differences
+        self.zR = 0.5*self.k0*(self.w0)**2        # Rayleigh range, ignoring horizontal/vertical differences
+#        print('\n ****** \n zR = ', self.zR)
 
         # pulse length
         self.tau_fwhm = _k.tau_fwhm               # FWHM laser pulse length [s]
@@ -80,12 +81,12 @@ class GaussHermite:
     # set the horizontal waist size [m]
     def set_waist_x(self, _waistX):
         # error handling; very small waist will cause performance issues
-        wFac = 20.
+        wFac = 4.0
         minSize = wFac*self.lambda0
         if _waistX >= minSize:
             self.waist_x  = _waistX
         else: 
-            message = 'waistX = ' + str(_waistX) + '; must be > ' + str(minSize)
+            message = 'waistX = ' + str(_waistX) + '; must be >= ' + str(minSize)
             raise Exception(message) 
         
         # error handling; require that deviations from w0 are small
@@ -102,12 +103,12 @@ class GaussHermite:
     # set the vertical waist size [m]
     def set_waist_y(self, _waistY):
         # error handling; very small waist will cause performance issues
-        wFac = 20.
+        wFac = 4.0
         minSize = wFac*self.lambda0
         if _waistY >= minSize:
             self.waist_y  = _waistY
         else: 
-            message = 'waistY = ' + str(_waistY) + '; must be > ' + str(minSize)
+            message = 'waistY = ' + str(_waistY) + '; must be >= ' + str(minSize)
             raise Exception(message) 
         
         # error handling; require that deviations from w0 are small
@@ -168,15 +169,15 @@ class GaussHermite:
     #   x,y,t can all be arrays, for a particle distribution with fixed z
     #   _z, the longitudinal coordinate, must always be a scalar.
     def evaluate_ex(self,xArray,yArray,_z,tArray):
-
-        # get the complex-valued envelope function
-        result = self.evaluate_envelope_ex(xArray,yArray,_z)
         
         # account for location of pulse center
         z_local = _z - self.z_center
 
+        # get the complex-valued envelope function
+        result = self.evaluate_envelope_ex(xArray, yArray, _z)
+
         # multiply by the time-dependent term
-        return result * np.exp((self.k0*z_local - self.omega0*tArray)*1j)
+        return result * np.exp((self.omega0*tArray - self.k0*z_local)*1j)
 
     # For now, we assume this is the polarization direction
     # Handling of arguments is flexible:
@@ -218,7 +219,7 @@ class GaussHermite:
         # radius at which the field amplitudes fall to exp(-1) of their axial values
         #     i.e., where the intensity values fall to exp(-2)
         wZ = self.w0 * math.sqrt(1+(_z/self.zR)**2)
-        pkdc('w(z)/w0 = ' + str(wZ/self.w0))
+#        pkdc('w(z)/w0 = ' + str(wZ/self.w0))
 
         # the radius squared
         rSq = np.power(xArray,2) + np.power(yArray,2)
@@ -236,12 +237,12 @@ class GaussHermite:
         arg_2 = 0.5*self.k0*invR*rSq
         exp_2 = np.exp(-1j*(arg_2 - psi_z))
 
-        pkdc(' k0 = ' + str(self.k0))
-        pkdc(' invR = ' + str(invR))
-        pkdc(' rSq = ' + str(rSq))
-        pkdc(' arg_2 = ' + str(arg_2))
-        pkdc(' psi_z = ' + str(psi_z))
-        pkdc(' Re[exp_2] = ' + str(np.real(exp_2)))
+#        pkdc(' k0 = ' + str(self.k0))
+#        pkdc(' invR = ' + str(invR))
+#        pkdc(' rSq = ' + str(rSq))
+#        pkdc(' arg_2 = ' + str(arg_2))
+#        pkdc(' psi_z = ' + str(psi_z))
+#        pkdc(' Re[exp_2] = ' + str(np.real(exp_2)))
 
         # return the complex valued result
         # here, we apply a longitudinal Gaussian profile
@@ -255,15 +256,15 @@ class GaussHermite:
     #   _z, the longitudinal coordinate, must always be a scalar.
     #   tArray can be a scalar (works) or a Numpy array (not tested)
     def evaluate_er(self,rArray,_z,tArray):
-
-        # get the complex-valued envelope function
-        result = self.evaluate_envelope_er(rArray,_z)
         
         # account for location of pulse center
         z_local = _z - self.z_center
 
+        # get the complex-valued envelope function
+        result = self.evaluate_envelope_er(rArray, _z)
+
         # multiply by the time-dependent term
-        return result * np.exp((self.k0*z_local - self.omega0*tArray)*1j)
+        return result * np.exp((self.omega0*tArray - self.k0*z_local)*1j)
 
     
     # Calculate the laser pulse envelope in radial r-z coordinates
@@ -279,7 +280,8 @@ class GaussHermite:
         # account for the waist location
         z_local = _z - self.z_center
         _z -= self.z_waist
-        
+#        pkdc('z_local, _z = ' + str(z_local) + ', ' + str(_z))
+       
         # determine whether xArray is really a Numpy array
         try:
             num_vals_r = rArray.size
@@ -295,7 +297,7 @@ class GaussHermite:
         # radius at which the field amplitudes fall to exp(-1) of their axial values
         #     i.e., where the intensity values fall to exp(-2)
         wZ = self.w0 * math.sqrt(1+(_z/self.zR)**2)
-        pkdc('w(z)/w0 = ' + str(wZ/self.w0))
+#        pkdc('w(z)/w0 = ' + str(wZ/self.w0))
 
         # the radius squared
         rSq = np.power(rArray,2)
@@ -310,50 +312,28 @@ class GaussHermite:
         psi_z = np.arctan(_z / self.zR)
         
         # 2nd exponential
-        arg_2 = 0.5*self.k0*invR*rSq
-        exp_2 = np.exp(-1j*(arg_2 - psi_z))
+        arg_2 = 0.5*self.k0*invR*rSq - psi_z
+        exp_2 = np.exp(-1j*arg_2)
 
-        pkdc(' k0 = ' + str(self.k0))
-        pkdc(' invR = ' + str(invR))
-        pkdc(' rSq = ' + str(rSq))
-        pkdc(' arg_2 = ' + str(arg_2))
-        pkdc(' psi_z = ' + str(psi_z))
-        pkdc(' Re[exp_2] = ' + str(np.real(exp_2)))
+#        pkdc(' k0 = ' + str(self.k0))
+#        pkdc(' invR = ' + str(invR))
+#        pkdc(' rSq = ' + str(rSq))
+#        pkdc(' arg_2 min/max = ' + str(np.min(arg_2)) + ', ' + str(np.max(arg_2)))
+#        pkdc(' psi_z = ' + str(psi_z))
+#        pkdc(' Re[exp_2] = ' + str(np.real(exp_2)))
 
         # return the complex valued result
         # here, we apply a longitudinal Gaussian profile
         return (self.w0 / wZ) * exp_1 * np.exp(-(z_local/self.L_fwhm)**2) * self.efield0 * exp_2
 
-    # For now, we assume this is the polarization direction
-    # Handling of arguments is flexible:
-    #   x,y,z,t can all be scalar, to evaluate at a single point.
-    #   x,y can both be arrays (same length) to evaluate on a mesh.
-    #   t can be an array, to evaluate at a sequence of times.
-    #   x,y,t can all be arrays, for a particle distribution with fixed z
-    #   z, the longitudinal coordinate, must always be a scalar.
-    #
-    # There is no longitudinal profile implemented here
-    
-    def eval_gh_ex(self,xArray,yArray,z,tArray):
-
-        # get the complex-valued envelope function
-        result = self.eval_gh_envelope_ex(xArray,yArray,z)
-
-        # multiply by the time-dependent term
-        result *= np.exp((self.omega0 * tArray - self.k0 * z)*1j)
-
-        # return only the real part
-        return np.real(result)*self.efield0
-
-    # For now, we assume this is the polarization direction
-    # Handling of arguments is flexible:
+    # This is old, untested code with no concept of a longitudinal envelope
     #   x,y,z can all be scalar, to evaluate at a single point.
     #   x,y can both be arrays (same length) to evaluate on a mesh.
     #   z, the longitudinal coordinate, must always be a scalar.
     #
     # The complicated logic below requires further testing.
     
-    def eval_gh_envelope_ex(self,xArray,yArray,z):
+    def eval_gh_ex(self,xArray,yArray,z):
 
         # assume array input; try to create temporary array
         try:

rslaser/utils/plot_fields.py
@@ -144,7 +144,7 @@ def plot_2d_zy(_zArr, _yArr, _pulse, _ax, _x=0., _t=0., _time_explicit=False, _n
     numZ = np.size(_zArr)
     numY = np.size(_yArr)
 
-    # Calculate Ex at the 2D array of x,y values
+    # Calculate Ex at the 2D array of z,y values
 #    zyEData = np.zeros((numZ, numY))
     zyEData = np.zeros((numY, numZ))
     if _time_explicit:
@@ -187,31 +187,6 @@ def plot_2d_zy(_zArr, _yArr, _pulse, _ax, _x=0., _t=0., _time_explicit=False, _n
             tick_values.append(i * del_level)
     cbar.set_ticks(tick_values)
 
-
-def plot_2d_zx(_zArr, _xArr, _pulse, _ax):
-    numX = np.size(_xArr)
-    minX = np.min(_xArr)
-    maxX = np.max(_xArr)
-        
-    numZ = np.size(_zArr)
-    minZ = np.min(_zArr)
-    maxZ = np.max(_zArr)
-
-    # specify y position for plot
-    yValue = 0.
-
-    # Calculate Ex at the 2D array of x,y values
-    zxEData = np.zeros((numX, numZ))
-    for iLoop in range(numZ):
-        zxEData[:, iLoop] = np.real(_pulse.evaluate_envelope_ex(_xArr, yValue, _zArr[iLoop]))
-
-    # generate the contour plot
-    _ax.axis([minZ, maxZ, minX, maxX])
-    _ax.set_xlabel('z [m]')
-    _ax.set_ylabel('x [m]')
-    _ax.set_title('ZX slice, at  y={0:4.2f} [{1}]'.format(yValue, '[m]'))
-    _ax.contourf(_xArr, _zArr, zxEData)
-
     
 def plot_2d_xy(_xArr, _yArr, _pulse, _ax, _z=0., _t=0., _time_explicit=False, _nlevels=40):
     """Generate a 2D contour plot of Ex in the transverse plane.
@@ -252,10 +227,12 @@ def plot_2d_xy(_xArr, _yArr, _pulse, _ax, _z=0., _t=0., _time_explicit=False, _n
     else:
         _ax.set_title('Ex (envelope) [V/m], at z={0:4.2f} [m]'.format(_z))
 
-    del_level = rspt.round_sig_fig(0.101*xyEData.max(), 3)
     n_cbar_labels = 10
-    max_level = n_cbar_labels * del_level
-    _levels = np.linspace(0., max_level, _nlevels)
+    min_level = rspt.round_sig_fig(1.01*min(xyEData.min(), 0.), 3)
+    max_level = rspt.round_sig_fig(1.01*xyEData.max(), 3)
+    del_level = rspt.round_sig_fig((max_level-min_level)/n_cbar_labels, 3)
+
+    _levels = np.linspace(min_level, max_level, _nlevels)
     contours = _ax.contourf(_xArr, _yArr, xyEData, _levels, extent='none')
     
     # generate the colorbar
@@ -264,5 +241,54 @@ def plot_2d_xy(_xArr, _yArr, _pulse, _ax, _z=0., _t=0., _time_explicit=False, _n
     cbar = plt.colorbar(contours, format='%3.2e', cax=_cax)
     tick_values = []
     for i in range(n_cbar_labels+1):
-        tick_values.append(i * del_level)
+        tick_values.append(min_level + i*del_level)
+    cbar.set_ticks(tick_values)
+
+        
+def plot_2d_zr(_zArr, _rArr, _pulse, _ax, _x=0., _t=0., _time_explicit=False, _nlevels=40):
+
+    numZ = np.size(_zArr)
+    numR = np.size(_rArr)
+
+    # Calculate Ex at the 2D array of z,r values
+    zrEData = np.zeros((numR, numZ))
+    if _time_explicit:
+        for i in range(numZ):
+            for j in range(numR):
+                zrEData[j, i] = np.real(_pulse.evaluate_er(_rArr[j], _zArr[i], _t))
+    else:
+        for i in range(numZ):
+            zrEData[:, i] = np.real(_pulse.evaluate_envelope_er(_rArr, _zArr[i]))
+
+    # generate the contour plot
+    _ax.clear()
+    _ax.axis([_zArr.min(), _zArr.max(), 0., _rArr.max()])
+    _ax.set_xlabel('z [m]')
+    _ax.set_ylabel('r [m]')
+    _ax.set_title('ZY slice, at  x={0:4.2f} [m]'.format(_x))
+
+    if _time_explicit:
+        del_level = rspt.round_sig_fig(0.202*zrEData.max(), 3)
+        n_cbar_labels = 10  # choose an even number
+        max_level = n_cbar_labels * del_level / 2
+        _levels = np.linspace(-max_level, max_level, _nlevels)
+    else:
+        del_level = rspt.round_sig_fig(0.101*zrEData.max(), 3)
+        n_cbar_labels = 10  # choose an even number
+        max_level = n_cbar_labels * del_level
+        _levels = np.linspace(0., max_level, _nlevels)
+        
+    contours = _ax.contourf(_zArr, _rArr, zrEData, _levels, extent='none')
+    
+    # generate the colorbar
+    divider = make_axes_locatable(_ax)
+    _cax = divider.append_axes("right", size="5%", pad=0.1)
+    cbar = plt.colorbar(contours, format='%3.2e', cax=_cax)
+    tick_values = []
+    if _time_explicit:
+        for i in range(n_cbar_labels+1):
+            tick_values.append(-max_level + i * del_level)
+    else:
+        for i in range(n_cbar_labels+1):
+            tick_values.append(i * del_level)
     cbar.set_ticks(tick_values)