b214a2cc078e3dc2a13fa14dbca53d088b3b03b4
gurhar1133 <ghns1133@gmail.com>
2022-03-02 22:15:25 +0000

    Fix #39: refact

--- tests/instantiation_test.py
+++ tests/instantiation_test.py
@@ -28,62 +28,7 @@ _LASER_PULSE_SLICE_DEFAULTS = PKDict(
     mx=0,
     my=0
 )
-
-def test_basic_instantiation1():
-    chirp = 0.0
-    k=PKDict(
-        phE=_PHE_DEFAULT,
-        nslice=3,
-        chirp=chirp,
-        # TODO (gurhar1133): format k {kv pairs ..., hermite kv pairs: {}, slice kv pairs: {}} ?
-        w0=.1,
-        a0=.01,
-        dw0x=0.0,
-        dw0y=0.0,
-        z_waist=_Z_WAIST_DEFAULT,
-        dzwx=0.0,
-        dzwy=0.0,
-        tau_fwhm=0.1 / const.c / math.sqrt(2.),
-        z_center=_Z_CENTER_DEFAULT,
-        x_shift = 0.,
-        y_shift=0.,
-        d_to_w=_Z_WAIST_DEFAULT - _Z_CENTER_DEFAULT,
-        slice_params=_LASER_PULSE_SLICE_DEFAULTS,
-    )
-    l = LaserPulse(k)
-    for s in l.slice:
-        if s.phE != l.phE:
-            raise AssertionError('LaserPulseSlice has different wavelength than pulse as a whole')
-
-
-def test_basic_instantiation2():
-    chirp = 0.01*_PHE_DEFAULT
-    k=PKDict(
-        phE=_PHE_DEFAULT,
-        nslice=3,
-        chirp=chirp,
-        # TODO (gurhar1133): format k {kv pairs ..., hermite kv pairs: {}, slice kv pairs: {}} ?
-        w0=.1,
-        a0=.01,
-        dw0x=0.0,
-        dw0y=0.0,
-        z_waist=_Z_WAIST_DEFAULT,
-        dzwx=0.0,
-        dzwy=0.0,
-        tau_fwhm=0.1 / const.c / math.sqrt(2.),
-        z_center=_Z_CENTER_DEFAULT,
-        x_shift = 0.,
-        y_shift=0.,
-        d_to_w=_Z_WAIST_DEFAULT - _Z_CENTER_DEFAULT,
-        slice_params=_LASER_PULSE_SLICE_DEFAULTS,
-    )
-    l = LaserPulse(k)
-    a = [s.phE for s in l.slice]
-    assert len(set(a)) == len(a)
-
-
-def test_basic_pulse_slice_instantiation():
-    k=PKDict(
+_LASER_PULSE_DEFAULTS = PKDict(
         phE=_PHE_DEFAULT,
         nslice=3,
         chirp=0,
@@ -101,8 +46,26 @@ def test_basic_pulse_slice_instantiation():
         y_shift=0.,
         d_to_w=_Z_WAIST_DEFAULT - _Z_CENTER_DEFAULT,
         slice_params=_LASER_PULSE_SLICE_DEFAULTS,
-    )
-    s = [LaserPulseSlice(i, k) for i in range(10)]
+)
+
+
+def test_basic_instantiation1():
+    l = LaserPulse(_LASER_PULSE_DEFAULTS)
+    for s in l.slice:
+        if s.phE != l.phE:
+            raise AssertionError('LaserPulseSlice has different wavelength than pulse as a whole')
+
+
+def test_basic_instantiation2():
+    k = _LASER_PULSE_DEFAULTS
+    k.chirp = 0.01*_PHE_DEFAULT
+    l = LaserPulse(k)
+    a = [s.phE for s in l.slice]
+    assert len(set(a)) == len(a)
+
+
+def test_basic_pulse_slice_instantiation():
+    s = [LaserPulseSlice(i, _LASER_PULSE_DEFAULTS) for i in range(10)]
 
 
 def test_slice_input_validators():
