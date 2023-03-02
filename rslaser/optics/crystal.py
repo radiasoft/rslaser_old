Fixed intensity calc and scaling of file data

rslaser/optics/crystal.py
@@ -525,8 +525,6 @@ class CrystalSlice(Element):
             b_x = np.linspace(b.mesh.xStart,b.mesh.xFin,b.mesh.nx)
             b_y = np.linspace(b.mesh.yStart,b.mesh.yFin,b.mesh.ny) 
 
-        original_total = np.sum(np.sum(temp_array))
-        #print('Mesh original total: ', original_total)
         if not (np.array_equal(a_x, b_x) and np.array_equal(a_y, b_y)):
         
             # Create the spline for interpolation
@@ -541,24 +539,14 @@ class CrystalSlice(Element):
             temp_array[:,b_y > np.max(a_y)] = 0.0
             temp_array[:,b_y < np.min(a_y)] = 0.0
 
-        post_interp_total = np.sum(np.sum(temp_array))
-        #if np.max(b_x) > np.max(a_x):
-        #    print('  increasing mesh size')
-        #    temp_array *= (original_total/post_interp_total) #Only scale if final mesh is larger than original
-        #elif np.max(b_x) < np.max(a_x):
-        #    print('  decreasing mesh size')
-        #print('Mesh post-interp total: ', np.sum(np.sum(temp_array)))
         return temp_array  
     
     def calc_gain(self,thisSlice):
-
-        #print('\nSlice ', thisSlice.slice_index)
         
         lp_wfr = thisSlice.wfr
 
         # Interpolate the excited state density mesh of the current crystal slice to
         # match the laser_pulse wavefront mesh
-        #print('\nInterp pop_inversion:')
         temp_pop_inversion = self._interpolate_a_to_b('pop_inversion', lp_wfr)
 
         # Calculate gain
@@ -567,7 +555,6 @@ class CrystalSlice(Element):
 
         # Interpolate the excited state density mesh of the current crystal slice to
         # match the laser_pulse wavefront mesh
-        #print('\nInterp n_photons:')
         thisSlice.n_photons_2d.mesh = self._interpolate_a_to_b(thisSlice.n_photons_2d, lp_wfr)  
         thisSlice.n_photons_2d.x = np.linspace(lp_wfr.mesh.xStart,lp_wfr.mesh.xFin,lp_wfr.mesh.nx)
         thisSlice.n_photons_2d.y = np.linspace(lp_wfr.mesh.yStart,lp_wfr.mesh.yFin,lp_wfr.mesh.ny)
@@ -587,12 +574,10 @@ class CrystalSlice(Element):
         change_pop_inversion = PKDict(
                                 mesh = change_pop_mesh,
                                 x = np.linspace(lp_wfr.mesh.xStart,lp_wfr.mesh.xFin,lp_wfr.mesh.nx),
-                                y = np.linspace(lp_wfr.mesh.yStart,lp_wfr.mesh.yFin,lp_wfr.mesh.ny)
-                                )
+                                y = np.linspace(lp_wfr.mesh.yStart,lp_wfr.mesh.yFin,lp_wfr.mesh.ny))
 
         # Interpolate the change to the excited state density mesh of the current crystal slice (change_pop_inversion)
         # so that it matches self.pop_inversion
-        #print('\nInterp change_pop_inversion:')
         change_pop_inversion.mesh = self._interpolate_a_to_b(change_pop_inversion, 'pop_inversion')
 
         # Update the pop_inversion_mesh

rslaser/pulse/pulse.py
@@ -291,7 +291,7 @@ class LaserPulseSlice(ValidatorBase):
                 ex_numpy[2*i+1] = ex_imag[i]
             
             # scale the wfr sensor data
-            self.wfs_norm_factor = 112.5059521158824
+            self.wfs_norm_factor = 3088.2025747914677
             
             # scale for slice location
             number_slices_correction = np.exp(-self._pulse_pos**2.0/(2.0 *self.sig_s)**2.0)
@@ -322,12 +322,14 @@ class LaserPulseSlice(ValidatorBase):
 
         # Note: assumes longitudinal gaussian profile when initializing
         
-        intensity = srwlib.array('f', [0]*self.wfr.mesh.nx*self.wfr.mesh.ny) # "flat" array to take 2D intensity data
-        srwl.CalcIntFromElecField(intensity, self.wfr, 0, 0, 3, self.wfr.mesh.eStart, 0, 0) #extracts intensity
+        # intensity = srwlib.array('f', [0]*self.wfr.mesh.nx*self.wfr.mesh.ny) # "flat" array to take 2D intensity data
+        # srwl.CalcIntFromElecField(intensity, self.wfr, 0, 0, 3, self.wfr.mesh.eStart, 0, 0) #extracts intensity
+        
+        # # Reshaping intensity data from flat to 2D array
+        # intens_2d = np.array(intensity).reshape((self.wfr.mesh.nx, self.wfr.mesh.ny), order='C').astype(np.float64)
 
-        # Reshaping intensity data from flat to 2D array
-        intens_2d = np.array(intensity).reshape((self.wfr.mesh.nx, self.wfr.mesh.ny), order='C').astype(np.float64)
-
+        intens_2d = srwutil.calc_int_from_elec(self.wfr) #extract 2d intensity
+        
         efield_abs_sqrd_2d = np.sqrt(const.mu_0 / const.epsilon_0) * 2.0 * intens_2d # [V^2/m^2]
 
         dx = (self.wfr.mesh.xFin - self.wfr.mesh.xStart)/self.wfr.mesh.nx